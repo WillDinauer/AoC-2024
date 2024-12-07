548328f235f2c1df209ff3f1ce4835a6bad0a4c2
WillDinauer <william.dinauer@gmail.com>
2024-12-07 00:23:20 -0500

    day 7 complete

--- day7.py
+++ day7.py
@@ -1,5 +1,5 @@
 def read_lines():
-    with open("inputs/day7_example.txt", "r") as f:
+    with open("inputs/day7.txt", "r") as f:
         line = f.readline()
         targets = []
         nums = []
@@ -26,7 +26,22 @@ def part1():
     for i, line in enumerate(nums):
         result += evaluate_line(targets[i], line)
     return result
+
+def special_recurse(target, line, idx, cur):
+    if idx == len(line):
+        return cur == target
+    return special_recurse(target, line, idx+1, cur+line[idx]) or special_recurse(target, line, idx+1, cur*line[idx]) or special_recurse(target, line, idx+1, int(str(cur) + str(line[idx])))
+
+def special_eval(target, line):
+    return target if special_recurse(target, line, 1, line[0]) else 0
         
+def part2():
+    targets, nums = read_lines()
+    result = 0
+    for i, line in enumerate(nums):
+        result += special_eval(targets[i], line)
+    return result
 
 if __name__ == "__main__":
     print(part1())
+    print(part2())
---
+++ test.py
@@ -0,0 +1,4 @@
+cur = 99
+next = 5
+cur = int(str(cur) + str(next))
+print(cur)
