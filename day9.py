48c6b7cee1f5e5e333febdd7b1004c540360d42b
WillDinauer <william.dinauer@gmail.com>
2024-12-09 11:23:13 -0500

    Day 9 complete

--- day9.py
+++ day9.py
@@ -3,9 +3,9 @@ def read_line():
         line = f.readline().strip()
         return [int(line[i]) for i in range(len(line))]
     
-def sequence_sum(d, x, p, i):
+def sequence_sum(id, length, pos, offset):
     # Sum of arithmetic sequence
-    return (d*x*(2*p+2*i+x-1))//2
+    return (id*length*(2*pos+2*offset+length-1))//2
 
 def part1():
     line = read_line()
@@ -15,6 +15,7 @@ def part1():
     r = len(line)-1
     unfinished = False
     while l < r:
+        unfinished = False
         result += sequence_sum(l//2, line[l], pos, 0)
         pos += line[l]
         i = 0
@@ -38,9 +39,30 @@ def part1():
         result += sequence_sum(l//2, line[l], pos, 0)
     return result
 
+def slot(line, i, pos):
+    for l in range(1, i, 2):
+        if line[l] >= line[i]:
+            res = sequence_sum(i//2, line[i], pos[l], 0)
+            line[l] -= line[i]
+            pos[l] += line[i]
+            return res
+    return 0
+
 def part2():
-    pass
-    
+    line = read_line()
+    result = 0
+    cur = 0
+    pos = {}
+    for i, num in enumerate(line):
+        pos[i] = cur
+        cur += num
+
+    for i in range(len(line)-1, -1, -2):
+        res = slot(line, i, pos)
+        if res == 0:
+            res = sequence_sum(i//2, line[i], pos[i], 0)
+        result += res
+    return result
 
 if __name__ == "__main__":
     print(part1())
