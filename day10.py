1ce38ec00e7ad025ef1eb99337a4ffd7cac284af
WillDinauer <william.dinauer@gmail.com>
2024-12-10 10:28:10 -0500

    Day 10 complete

--- day10.py
+++ day10.py
@@ -33,8 +33,27 @@ def part1():
                 result += travel(grid, r, c, 0, [])
     return result
 
+def new_rating(grid, r, c, cur):
+    if r < 0 or c < 0 or r >= len(grid) or c >= len(grid) or grid[r][c] != cur:
+        return 0
+    if cur == 9:
+        return 1
+    temp = grid[r][c]
+    grid[r][c] = -1
+    res = 0
+    for i in range(4):
+        res += new_rating(grid, r+dr[i], c+dc[i], cur+1)
+    grid[r][c] = temp
+    return res
+
 def part2():
-    pass
+    grid = read_grid()
+    result = 0
+    for r in range(len(grid)):
+        for c in range(len(grid)):
+            if grid[r][c] == 0:
+                result += new_rating(grid, r, c, 0)
+    return result
     
 if __name__ == "__main__":
     print(part1())
