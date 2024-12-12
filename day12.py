027ef8be1e9e9126e25a1d5057d7da940babeb80
WillDinauer <william.dinauer@gmail.com>
2024-12-12 01:41:13 -0500

    Completed day 12

--- day12.py
+++ day12.py
@@ -10,7 +10,7 @@ def read_grid():
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 
-def dfs(grid, v, r, c, val, p):
+def dfs_p1(grid, v, r, c, val):
     if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]) or grid[r][c] != val:
         return 0, 1
     if v[r][c] == 1:
@@ -19,15 +19,14 @@ def dfs(grid, v, r, c, val, p):
     area = 1
     perimeter = 0
     for i in range(4):
-        sa, sp = dfs(grid, v, r+dr[i], c+dc[i], val, p)
+        sa, sp = dfs_p1(grid, v, r+dr[i], c+dc[i], val)
         area += sa
         perimeter += sp
     return area, perimeter
 
 def compute_region(grid, v, r, c):
     val = grid[r][c]
-    perimeter = []
-    area, perimeter = dfs(grid, v, r, c, val, perimeter)
+    area, perimeter = dfs_p1(grid, v, r, c, val)
     return area * perimeter
 
 def part1():
@@ -40,8 +39,51 @@ def part1():
                 result += compute_region(grid, v, r, c)
     return result
 
+
+def dfs_p2(grid, v, r, c, val, cache, dir):
+    if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]) or grid[r][c] != val:
+        if dir == 0 or dir == 1:
+            cache[dir].append([r, c])
+        else:
+            cache[dir].append([c, r])
+        return 0
+    if v[r][c] == 1:
+        return 0
+    v[r][c] = 1
+    area = 1
+    for i in range(4):
+        area += dfs_p2(grid, v, r+dr[i], c+dc[i], val, cache, i)
+    return area
+
+def compute_sides(cache):
+    sides = 0
+    for direction in cache:
+        sides += 1
+        line = cache[direction]
+        line.sort()
+        for i in range(len(line)-1):
+            if line[i+1][0] != line[i][0]:
+                sides += 1
+            elif line[i+1][1] > line[i][1] + 1:
+                sides += 1
+    return sides
+
+def side_finder(grid, v, r, c):
+    cache = {0:[], 1:[], 2:[], 3:[]}
+    val = grid[r][c]
+    area = dfs_p2(grid, v, r, c, val, cache, -1)
+    sides = compute_sides(cache)
+    return area * sides
+
 def part2():
-    pass
+    grid = read_grid()
+    v = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
+    result = 0
+    for r in range(len(grid)):
+        for c in range(len(grid[r])):
+            if v[r][c] == 0:
+                result += side_finder(grid, v, r, c)
+    return result
 
 if __name__ == "__main__":
     print(part1())
