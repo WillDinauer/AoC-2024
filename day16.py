6b2555093cbce0c3fe19884d8100890143066a66
WillDinauer <william.dinauer@gmail.com>
2024-12-16 01:54:38 -0500

    Day16 complete

--- day16.py
+++ day16.py
@@ -14,49 +14,65 @@ def read_input():
             line = f.readline().strip()
         return grid, start, end
 
-def print_grid(grid, start, end, prev):
-    path = []
-    cur = prev[end[0], end[1]]
-    while cur != start:
-        path.append(cur)
-        cur = prev[cur[0], cur[1]]
+def print_grid(grid, path):
     for r in range(len(grid)):
         for c in range(len(grid[0])):
-            print("X" if [r, c] in path else grid[r][c], end="")
+            print("O" if [r, c] in path else grid[r][c], end="")
         print()
-
 
 dr = [-1, 0, 1, 0]
 dc = [0, 1, 0, -1]
 
-def part1():
+if __name__ == "__main__":
     grid, start, end = read_input()
+    score = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
     visited = set()
+    prev = {}
     q = []
     heapq.heapify(q)
-    heapq.heappush(q, [0, start[0], start[1], 1])
+    heapq.heappush(q, [0, start[0], start[1], 1, None])
+    p1 = -1
     while len(q) > 0:
-        cur, r, c, dir = heapq.heappop(q)
+        cur, r, c, dir, p = heapq.heappop(q)
         if (r, c, dir) in visited:
+            if cur == score[r][c] and p not in prev[node]:
+                prev[node].append(p)
             continue
-        
-        if [r, c] == end:
-            return cur
+
+        node = (cur, r, c)
+        if node not in prev:
+            prev[node] = []
+        prev[node].append(p)
+
+        if [r, c] == end and p1 < 0:
+            p1 = cur
         
         visited.add((r, c, dir))
+        score[r][c] = cur
         nr = r + dr[dir]
         nc = c + dc[dir]
         if grid[nr][nc] == "." or grid[nr][nc] == "E":
-            heapq.heappush(q, [cur+1, nr, nc, dir])
+            heapq.heappush(q, [cur+1, nr, nc, dir, node])
 
         for rotation in [-1, 1]:
-            heapq.heappush(q, [cur + 1000, r, c, (dir + rotation)%4])
+            heapq.heappush(q, [cur + 1000, r, c, (dir + rotation)%4, node])
 
-    return float('inf')
-            
-def part2():
-    pass
+    cur = [(p1, end[0], end[1])]
+    seen = []
+    path = []
+    while len(cur) > 0:
+        new_cur = []
+        for pt in cur:
+            if pt == None:
+                break
+            if pt not in seen:
+                seen.append(pt)
+                if [pt[1], pt[2]] not in path:
+                    path.append([pt[1], pt[2]])
+                for np in prev[pt]:
+                    new_cur.append(np)
+        cur = new_cur
+    print_grid(grid, path)
 
-if __name__ == "__main__":
-    print(part1())
-    print(part2())
+    print(f"p1: {p1}")
+    print(f"p2: {len(path)}")
