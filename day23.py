ea3064912ab17c9397dc538411e239eeab81d834
WillDinauer <77174175+WillDinauer@users.noreply.github.com>
2024-12-23 11:34:37 -0500

    Day23 complete

--- day23.py
+++ day23.py
@@ -1,5 +1,15 @@
+import time
+import copy
+
 lines = open("inputs/day23.txt", "r").read().split("\n")
 graph = [[0 for _ in range(26*26)] for _ in range(26*26)]
+
+def translate(cc):
+    res = ""
+    cc.sort()
+    for c in cc:
+        res += chr(ord('a')+c//26) + chr(ord('a')+c%26) + ","
+    return res
 
 def cs(cp):
     return (ord(cp[0])-ord('a'))*26 + ord(cp[1]) - ord('a')
@@ -11,16 +21,39 @@ for line in lines:
     graph[a][b] = 1
     graph[b][a] = 1
 
-tl = (ord('t')-ord('a'))*26
-tr = tl + 26
+left = (ord('t')-ord('a'))*26
+right = left + 26
+
+cur = []
 p1 = 0
-seen = set()
-for i in range(tl, tr):
-    for j in range(len(graph)):
-        for k in range(len(graph)):
-            cur = [i, j, k]
-            cur.sort()
-            if not tuple(cur) in seen and graph[i][j] == 1 and graph[i][k] == 1 and graph[j][k] == 1:
-                p1 += 1
-                seen.add(tuple(cur))
+for i in range(len(graph)):
+    for j in range(i+1, len(graph)):
+        for k in range(j+1, len(graph)):
+            if graph[i][j] == 1 and graph[i][k] == 1 and graph[j][k] == 1:
+                cur.append([i, j, k])
+                if (i >= left and i < right) or (j >= left and j < right) or (k >= left and k < right):
+                    p1 += 1
 print(f"p1: {p1}")
+
+
+def validate(graph, g, i):
+    for r in g:
+        if graph[r][i] == 0:
+            return False
+    return True
+
+best = 2
+prev = cur
+while len(cur) > 0:
+    ng = []
+    best += 1
+    for g in cur:
+        for i in range(g[-1]+1, len(graph)):
+            if validate(graph, g, i):
+                cg = copy.deepcopy(g)
+                cg.append(i)
+                ng.append(cg)
+    prev = cur
+    cur = ng
+
+print(f"p2: {translate(prev[0])[:-1]}")
